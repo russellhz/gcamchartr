281961a77b68e51a501ef7635bc8a0e36e354f8f
Russell Horowitz <russell.horowitz@pnnl.gov>
2017-06-20 14:08:54 -0400

    Plot by regions

--- R/bar_chart_plot.R
+++ R/bar_chart_plot.R
@@ -4,8 +4,10 @@
 #' @param data Dataframe/tibble of processed GCAM data
 #' @param output_dir Output directory to save charts to
 #' @param fill_col Column name, in quotes, to color by. Defaults to attribute of "fill" for processed data.
+#' @param diff If true, adds subtitle to plot and DIFF to output name
 #' @param colors Vector of colors for chart. Defaults to attribute of "colors" for processed data
 #' @param break_size Interval of years to print on x axis, starting with minimum year in data. Default is 10 years
+#' @param region String of region name to filter by
 #' @keywords bar chart
 #' @import dplyr ggplot2 tidyr
 #' @export
@@ -14,7 +16,17 @@
 #' bar_chart_plot(data[["primary_energy"]], output_dir = "/outputs")
 
 bar_chart_plot <- function(data, output_dir, fill_col = attributes(data)$fill, colors = attributes(data)$colors, diff = F,
-                           width = 10, height = 7, break_size = 10){
+                           region = NULL, width = 10, height = 7, break_size = 10){
+  # Filter by region if necessary
+  if (!is.null(region)){
+    if (length(unique(data$region)) > 1){
+    region_name <- region
+    data <- data %>% filter(region == region_name)
+    }else{
+      warning(paste("There is only one region in data for", attributes(data)$query))
+    }
+  }
+
   # Make sure output directory exists and ends in "/"
   if (substr(output_dir, nchar(output_dir), nchar(output_dir)) != "/"){
     output_dir <- paste0(output_dir, "/")
@@ -48,8 +60,16 @@ bar_chart_plot <- function(data, output_dir, fill_col = attributes(data)$fill, c
       labs(subtitle = paste("Change from", unique(data$diff_scenario))) +
       theme(plot.subtitle = element_text(size = 14, face = "italic"))
 
-  ggsave(paste0(output_dir, "DIFF_", attributes(data)$query,".png"), plot = all_scen_plot, width = width, height = height)
-  } else{
-        ggsave(paste0(output_dir, attributes(data)$query,".png"), plot = all_scen_plot, width = width, height = height)
+    filename <- paste0(output_dir, "DIFF_", attributes(data)$query,".png")
+  } else{ filename <- paste0(output_dir, attributes(data)$query,".png")}
+
+  if (!is.null(region)){
+    if (exists("region_name")){
+      start <- regexpr(attributes(data)$query, filename)[1]
+      filename <- paste0(substr(filename, 1, start - 1), region_name, "_", substr(filename, start, nchar(filename)))
     }
+  }
+
+  ggsave(filename, plot = all_scen_plot, width = width, height = height)
+
 }
