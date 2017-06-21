87fc654b3b0b5f0906a2dd1886d4223b55ac8395
Russell Horowitz <russell.horowitz@pnnl.gov>
2017-06-21 13:42:23 -0400

    line_chart_plot

--- R/line_chart_plot.R
+++ R/line_chart_plot.R
@@ -1,27 +1,26 @@
-#' bar_chart_plot
+#' line_chart_plot
 #'
-#' Create bar chart of processed data
+#' Create line chart of processed data
 #' @param data Dataframe/tibble of processed GCAM data
 #' @param output_dir Output directory to save charts to
-#' @param fill_col Column name, in quotes, to color by. Defaults to attribute of "fill" for processed data.
+#' @param fill_col Column name, in quotes, to color by. Default is "scenario". To use other column, may need to add facet.
 #' @param diff If true, adds subtitle to plot and DIFF to output name
-#' @param colors Vector of colors for chart. Defaults to attribute of "colors" for processed data
 #' @param break_size Interval of years to print on x axis, starting with minimum year in data. Default is 10 years
 #' @param region String of region name to filter by
-#' @keywords bar chart
+#' @keywords line chart
 #' @import dplyr ggplot2 tidyr
 #' @export
 #' @examples
-#' bar_chart_plot(df, output_dir = "/outputs", fill = "technology", colors = c("red", "blue"))
-#' bar_chart_plot(data[["primary_energy"]], output_dir = "/outputs")
+#' line_chart_plot(df, output_dir = "/outputs")
+#' line_chart_plot(data[["primary_energy"]], output_dir = "/outputs")
 
-bar_chart_plot <- function(data, output_dir, fill_col = attributes(data)$fill, colors = attributes(data)$colors, diff = F,
+line_chart_plot <- function(data, output_dir, fill_col = "scenario", diff = F,
                            region = NULL, width = 10, height = 7, break_size = 10){
   # Filter by region if necessary
   if (!is.null(region)){
     if (length(unique(data$region)) > 1){
-    region_name <- region
-    data <- data %>% filter(region == region_name)
+      region_name <- region
+      data <- data %>% filter(region == region_name)
     }else{
       warning(paste("Data only exists for region",unique(data$region), "in", attributes(data)$query))
     }
@@ -35,33 +34,28 @@ bar_chart_plot <- function(data, output_dir, fill_col = attributes(data)$fill, c
 
   if ("Units" %in% names(data)) {y_axis <- unique(data$Units)}
 
-  all_scen_plot <- ggplot(data, aes_string("year", "value", fill = fill_col)) +
-    geom_bar(stat="identity", position = position_stack(reverse = TRUE)) +
-    scale_fill_manual(values = colors) +
-    guides(fill = guide_legend(reverse = TRUE, title = stringr::str_to_title(fill_col))) +
+  # Data needs to be aggregated to only the fill(color) column and year
+  data <- data %>% group_by_(fill_col, "year") %>% summarise(value = sum(value))
+
+  all_scen_plot <- ggplot(data, aes_string("year", "value", color = fill_col)) +
+    geom_line() +
+    guides(color = guide_legend(title = stringr::str_to_title(fill_col))) +
     labs(y = y_axis, title = attributes(data)$query) +
     theme(plot.title = element_text(size = 18, face = "bold"),
           axis.title.x = element_text(size = 14),
           axis.title.y = element_text(size = 14),
           axis.text.x = element_text(size = 12, angle = 45),
           axis.text.y = element_text(size = 12),
-          strip.text = element_text(size = 14),
           legend.title = element_text(size = 14)) +
     scale_x_continuous(breaks = seq(min(data$year), max(data$year),break_size))
-
-
-  if ("scenario" %in% names(data) & length(data$scenario) > 1){
-    all_scen_plot <- all_scen_plot +
-      facet_wrap(~scenario, ncol = 2)
-  }
 
   if (diff == T){
     all_scen_plot <- all_scen_plot +
       labs(subtitle = paste("Change from", unique(data$diff_scenario))) +
       theme(plot.subtitle = element_text(size = 14, face = "italic"))
 
-    filename <- paste0(output_dir, "DIFF_", attributes(data)$query,".png")
-  } else{ filename <- paste0(output_dir, attributes(data)$query,".png")}
+    filename <- paste0(output_dir, "DIFF_", attributes(data)$query,"_line.png")
+  } else{ filename <- paste0(output_dir, attributes(data)$query,"_line.png")}
 
   if (!is.null(region)){
     if (exists("region_name")){
